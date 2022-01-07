1267eb8273f4453f9a3170ab7ed7d1fb12ce2b30
NullBackspace <jandomalaon@gmail.com>
2022-01-07 15:33:39 -0500

    Created menu theme and added on main menu. Started module selection menu.

---
+++ menu/main_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+func _ready():
+	pass
+
+
+func _on_Play_pressed():
+	get_tree().change_scene()
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
+
+
+func _on_Settings_pressed():
+	pass # Replace with function body.
+
+
+
