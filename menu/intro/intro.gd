6ab5cff5a97d44c58c6ed2b17774ae99714498d9
jandomalaon@gmail.com <jandomalaon@gmail.com>
2022-01-09 23:14:38 -0500

    Added main menu particles and vignettes

--- menu/intro/intro.gd
+++ menu/intro/intro.gd
@@ -1,5 +1,13 @@
 extends Control
 
 
+func _ready() -> void:
+	set_process_input(true)
+
+func _input(event) -> void:
+	# Skip instantly to main menu
+	if (event.is_action_pressed("ui_accept")):
+		get_tree().change_scene("res://menu/main_menu.tscn")
+
 func go_to_main_menu() -> void:
 	get_tree().change_scene("res://menu/main_menu.tscn")
